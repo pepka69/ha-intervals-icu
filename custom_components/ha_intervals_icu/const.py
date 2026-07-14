af96a27e3ad11a7528c13bfa4c89f465f7b1fdd5
Alex <63163184+pepka69@users.noreply.github.com>
2026-07-14 15:27:57 +0200

    Create const.py

---
+++ custom_components/ha_intervals_icu/const.py
@@ -0,0 +1,16 @@
+"""Constants for ha-intervals-icu."""
+
+DOMAIN = "ha_intervals_icu"
+
+NAME = "ha-intervals-icu"
+
+CONF_ATHLETE_ID = "athlete_id"
+CONF_API_KEY = "api_key"
+
+DEFAULT_SCAN_INTERVAL = 900
+
+API_BASE_URL = "https://intervals.icu/api/v1"
+
+ATTRIBUTION = "Data provided by Intervals.icu"
+
+LOGGER_NAME = "custom_components.ha_intervals_icu"
