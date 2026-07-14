9f598a7a834101739f1d26dae23624b7c99f2af4
Alex <63163184+pepka69@users.noreply.github.com>
2026-07-14 16:12:56 +0200

    Update integration constants

--- custom_components/ha_intervals_icu/const.py
+++ custom_components/ha_intervals_icu/const.py
@@ -1,16 +1,19 @@
 """Constants for ha-intervals-icu."""
 
+from __future__ import annotations
+
+import logging
+
+
 DOMAIN = "ha_intervals_icu"
 
-NAME = "ha-intervals-icu"
+LOGGER_NAME = "custom_components.ha_intervals_icu"
+
+LOGGER = logging.getLogger(LOGGER_NAME)
 
 CONF_ATHLETE_ID = "athlete_id"
 CONF_API_KEY = "api_key"
 
 DEFAULT_SCAN_INTERVAL = 900
 
-API_BASE_URL = "https://intervals.icu/api/v1"
-
 ATTRIBUTION = "Data provided by Intervals.icu"
-
-LOGGER_NAME = "custom_components.ha_intervals_icu"
